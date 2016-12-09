e94d8d8c1c20cd4de7948967cf385c003d55a2b1
秦莉莎 <980004531@qq.com>
2016-12-09 16:45:13 +0800

    frist commit I have finished the HomeActivity,but next ,I want to change activity to fragment.

---
+++ app/src/main/java/com/lisa/administrator/sixgold/activity/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.lisa.administrator.sixgold.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lisa.administrator.sixgold.R;
+
+/**
+ * 主页面
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/lisa/administrator/sixgold/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.lisa.administrator.sixgold.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lisa.administrator.sixgold.R;
+
+/**
+ * 闪屏页面
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
